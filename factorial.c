a7ec46d455613cbe1653a10d98177c9dc9608963
umidah <angelo_parco@dlsu.edu.ph>
2021-04-17 11:11:13 +0800

    added factorial code

--- factorial.c
+++ factorial.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-
 /** Calculates the factorial of n. It is defined as:
  * n! = n * (n -1) * (n - 2) * (n - 3) * (n -4)
  * @param n = initial value of n
  * @return calculated result of n!
  */
-int factorial (int n) {
-    // ADD CODE HERE
-    return 0; // EDIT THIS
+int factorial(int n) {
+  if (n == 0)
+    return 1;
+  else {
+    int fact = 1;
+    for (int i = n; i > 0; i--) {
+      fact *= i;
+    }
+    return fact;
+  }
 }
 
-
-
 int main(void) {
+  int k = 5;
 
-    int k=5;
+  printf("%d! = %d\n", k, factorial(k));
+  k = 8;
+  printf("%d! = %d\n", k, factorial(k));
+  k = 10;
+  printf("%d! = %d\n", k, factorial(k));
 
-    printf("%d! = %d\n", k,factorial(k));
-    k = 8;
-    printf("%d! = %d\n", k,factorial(k));
-    k = 10;
-    printf("%d! = %d\n", k,factorial(k));
-
-    return 0;
+  return 0;
 }
